91b16f0fa58c5e5081d619ff6f115a8687002467
Guennadi Maximov Cortés <g.maxc.fox@protonmail.com>
2023-02-17 14:19:59 -0600

    "fix(build): Changed shebang to standard.

--- build.bash
+++ build.bash
@@ -1,3 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# AUTHOR(S): Luis Valencia Guennadi Maximov
 
 g++ -pedantic-errors -Werror -Wall -Weffc++ -Wextra -Wsign-conversion -std=c++20 src/window.cpp -o bin/window
