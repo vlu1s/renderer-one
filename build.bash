c11754d9b233fe7e00af622e925fe2ef8e3a413d
Guennadi Maximov Cortés <g.maxc.fox@protonmail.com>
2023-02-17 14:53:32 -0600

    update(build): Shebang correction 2.

--- build.bash
+++ build.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-# AUTHOR(S): Luis Valencia Guennadi Maximov
+# AUTHOR(S): Luis Valencia, Guennadi Maximov
 
 g++ -pedantic-errors -Werror -Wall -Weffc++ -Wextra -Wsign-conversion -std=c++20 src/window.cpp -o bin/window
