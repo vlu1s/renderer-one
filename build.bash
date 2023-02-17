6131beea3350dbe2215affb88874f744c69dba07
Luis Arturo Ramos Valencia <vluis@protonmail.com>
2023-02-17 14:50:23 -0600

    Open a window using GLFW.

--- build.bash
+++ build.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ -pedantic-errors -Werror -Wall -Weffc++ -Wextra -Wsign-conversion -std=c++20 src/window.cpp -o bin/window
+g++ -pedantic-errors -Werror -Wall -Weffc++ -Wextra -Wsign-conversion -std=c++20 src/window.cpp -o bin/window -lglfw
--- src/window.cpp
+++ src/window.cpp
@@ -1,6 +1,41 @@
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+
+const uint32_t WIDTH = 1280;
+const uint32_t HEIGHT = 720;
 
 int main()
 {
+	/** Initialize window **/
+	GLFWwindow* window;
+	/* Initialize glfw library */
+	if(!glfwInit())
+	{
+		// TODO(luis): loggging 
+		return -1;
+	}
+	/* Not an OpenGL window */
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	/* Create a windowed mode window */
+	window = glfwCreateWindow(WIDTH, HEIGHT, "Renderer One", NULL, NULL);
+	if(!window)
+	{
+		// TODO(luis): logging
+		glfwTerminate();
+		return -1;
+	}
+
+	/** TODO(luis): Initialize Vulkan? **/
+
+	/** Main Loop **/
+	while(!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	/** Window cleanup **/
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
 	return 0;
 }
